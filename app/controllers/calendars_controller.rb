39ecc0d7fb99a87a5eef62f03dc94162391d282f
ikekou0015 <ikekou0015@gmail.com>
2020-10-20 16:23:27 +0900

    lessue4.5.6の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    
+    # binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -36,12 +36,20 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+      wday_num = Date.today.wday + x #を利用して添字となる数値を得る
 
-
-      if 7.times do |wdays|
-        wday_num = wday_num - 7 
+      #もしもwday_numが7以上であれば、7を引く
+      if wday_num >= 7     
+          wday_num = wday_num - 7
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday:(@wday_num)}
-      @week_days.push(days)
-    end
 
+     # each do |wdays|
+     #   wday_num = wday_num - 7 
+     # end
+    #end
+  #end
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
+      @week_days.push(days)
+   end
+  end
+end
